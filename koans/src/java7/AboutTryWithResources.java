a4262ce900437c68c5585b706baed518151647d1
Ryan Ovrevik <rovrevik@ovrevik.com>
2019-11-16 17:41:49 -0500

    complete java7

--- koans/src/java7/AboutTryWithResources.java
+++ koans/src/java7/AboutTryWithResources.java
@@ -33,57 +33,44 @@ public class AboutTryWithResources {
 
     @Koan
     public void lookMaNoClose() {
-        String str = "first line"
-                + System.lineSeparator()
-                + "second line";
+        String str = "first line" + System.lineSeparator() + "second line";
         InputStream is = new ByteArrayInputStream(str.getBytes());
         String line;
         /* BufferedReader implementing @see java.lang.AutoCloseable interface */
-        try (BufferedReader br =
-                     new BufferedReader(
-                             new InputStreamReader(is))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
             line = br.readLine();
             //br guaranteed to be closed
         } catch (IOException e) {
             line = "error";
         }
-        assertEquals(line, __);
+        assertEquals(line, "first line");
     }
 
     @Koan
     public void lookMaNoCloseWithException() throws IOException {
         String line = "no need to close readers";
-        try (BufferedReader br =
-                     new BufferedReader(
-                             new FileReader("I do not exist!"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("I do not exist!"))) {
             line = br.readLine();
         } catch (FileNotFoundException e) {
             line = "no more leaking!";
         }
-        assertEquals(line, __);
+        assertEquals(line, "no more leaking!");
     }
 
     @Koan
     public void lookMaNoCloseWithMultipleResources() throws IOException {
-        String str = "first line"
-                + System.lineSeparator()
-                + "second line";
+        String str = "first line" + System.lineSeparator() + "second line";
         InputStream is = new ByteArrayInputStream(str.getBytes());
         String line;
         //multiple resources in the same try declaration
-        try (BufferedReader br =
-                     new BufferedReader(
-                             new FileReader("I do not exist!"));
-             BufferedReader brFromString =
-                     new BufferedReader(
-                             new InputStreamReader(is))
-        ) {
+        try (BufferedReader br = new BufferedReader(new FileReader("I do not exist!"));
+             BufferedReader brFromString =new BufferedReader(new InputStreamReader(is))) {
             line = br.readLine();
             line += brFromString.readLine();
         } catch (IOException e) {
             line = "error";
         }
-        assertEquals(line, __);
+        assertEquals(line, "error");
     }
 
     @Koan
@@ -96,13 +83,12 @@ public class AboutTryWithResources {
         } catch (CloseException e) {
             message += e.getMessage();
         }
-        assertEquals(message, __);
+        assertEquals(message, "Exception thrown while working Exception thrown while closing");
     }
 
 
     public void bar() throws CloseException, WorkException {
-        try (AutoClosableResource autoClosableResource =
-                     new AutoClosableResource()) {
+        try (AutoClosableResource autoClosableResource = new AutoClosableResource()) {
             autoClosableResource.foo();
         }
     }
